e9cb9b2ed84b6e11bad6b22848b951e5cfde9fb9
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2025-08-13 12:11:31 -0300

    update project route params

--- support-ticket/src/middlewares/routeHandler.js
+++ support-ticket/src/middlewares/routeHandler.js
@@ -1,10 +1,10 @@
 import { routes } from "../routes/index.js";
 import { Database } from "../database/database.js";
 
-const database = new Database()
+const database = new Database();
 export function routeHandler(request, response) {
   const route = routes.find((route) => {
-    return route.method === request.method && route.path === request.url;
+    return route.method === request.method && route.path.test(request.url);
   });
 
   if (route) {
