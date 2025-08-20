e89a5fdb9f6905edaac51211ae8a1df8d9950ece
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2025-08-20 13:22:43 -0300

    update in routes, regex, middlewares

---
+++ primeira-Api/src/routes.js
@@ -0,0 +1,28 @@
+import { parseRoutePath } from "./utils/parseRoutePath.js";
+
+export const routes = [
+  {
+    method: "GET",
+    path: "/products",
+    controller: (request, response) => {
+      return response.end(JSON.stringify("Lista de produtos"));
+    },
+  },
+  {
+    method: "POST",
+    path: "/products",
+    controller: (request, response) => {
+      return response.writeHead(201).end(JSON.stringify(request.body));
+    },
+  },
+  {
+    method: "DELETE",
+    path: "/products/:id",
+    controller: (request, response) => {
+      return response.writeHead(201).end(JSON.stringify("Removido"));
+    },
+  },
+].map((route) => ({
+  ...route,
+  path: parseRoutePath(route.path),
+}));
