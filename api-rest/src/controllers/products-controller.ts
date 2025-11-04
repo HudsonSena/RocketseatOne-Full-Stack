c6d4b6fee3d71d2f6ccbf47689d38e9695747b3e
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2025-11-04 17:22:47 -0300

    update project zod validations

--- api-rest/src/controllers/products-controller.ts
+++ api-rest/src/controllers/products-controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { AppError } from "../utils/app-error";
+import { z } from "zod";
 
 class ProductsController {
   //máximo 5 métodos
@@ -8,12 +9,15 @@ class ProductsController {
   }
 
   create(request: Request, response: Response) {
-    const { name, price } = request.body;
-    if (!name) throw new AppError("Nome é obrigatório");
-    if (name.trim().length === 0) throw new AppError("Nome não pode ser vazio");
-    if (!price) throw new AppError("Preço é obrigatório");
-    if (price < 0) throw new AppError("Preço não pode ser negativo");
+    const bodySchema = z.object({
+      name: z
+      .string({ required_error: "Name is required" })
+      .trim()
+      .min(6, { message: "Name must be 6 or more characters"}),
+      price: z.number({ required_error: "Price is required" }).positive({ message: "Price must be positive"})
+    });
 
+    const { name, price } = bodySchema.parse(request.body)
     response.status(201).json({ name, price, user_id: request.user_id });
   }
 }
